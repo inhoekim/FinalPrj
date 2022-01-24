8ac28cf9824f0bdf2a1abbe86395bc98b1a2e869
sang <djs9393@naver.com>
2022-01-24 16:46:08 +0900

    board_insert summernote 설정 및 controller

--- FinalPrj/src/main/java/com/spring/ott/service/PostService.java
+++ FinalPrj/src/main/java/com/spring/ott/service/PostService.java
@@ -1,5 +1,20 @@
 package com.spring.ott.service;
 
-public class PostService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.spring.ott.vo.FilesVo;
+import com.spring.ott.vo.PostVo;
+
+import data.mybatis.mapper.PostMapper;
+
+@Service
+public class PostService {
+	@Autowired PostMapper mapper;
+	public int postInsert(PostVo vo) {
+		return mapper.postInsert(vo);
+	}
+	public int fileInsert(FilesVo vo) {
+		return mapper.FileInsert(vo);
+	}
 }
