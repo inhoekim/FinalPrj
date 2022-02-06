2af4a75b8be078c039e02a7312901dae196eee30
KimInHoe <inhoe.kim95@gmail.com>
2022-02-06 22:42:31 +0900

    img 파일 추가

--- FinalPrj/src/main/java/com/spring/ott/controller/MyPartyController.java
+++ FinalPrj/src/main/java/com/spring/ott/controller/MyPartyController.java
@@ -1,12 +1,27 @@
 package com.spring.ott.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.spring.ott.service.MatchingCheckService;
+
 @Controller
 public class MyPartyController {
+	@Autowired MatchingCheckService matchingCheckService;
+	
 	@GetMapping("/autoMatch/myParty")
-	public String myParty() {
-		return "home/myParty.tiles";
+	public String myParty(Principal principal) {
+		int page = matchingCheckService.matchingCheck(principal.getName());
+		if(page == 1) {
+			return "automatching/myPartyLeader.tiles";
+		}else if (page == 2) {
+			return "automatching/myParty.tiles";
+		}else if (page == 3) {
+			return "automatching/wating.tiles";
+		}
+		return "automatching/startMatching.tiles";
 	}
 }
---
+++ FinalPrj/src/main/webapp/WEB-INF/views/automatching/startMatching.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<div class="myParty_container" style="min-height: 800px;">
+    <div style="margin: 50px 0;">
+        <div style="font-size: 30px;">
+            <b>예시</b> 님은<br>가입한 파티가 아직 존재하지않아요
+            <img src="#">
+        </div>
+        <div style="margin:20px 0;">
+            지금 <span style="color: #53e3ac"><b>파티매칭</b></span>을 시작하고 편리하게 계정을 공유해보세요!
+            <img src="#">
+        </div>
+    </div>
+    <div style="text-align: center; margin: 100px 0;">
+        <img src="#" style="">
+    </div>
+    <div class="confirm_button" style="width:60%; margin: auto; padding: 10px; font-size: 20px; text-align: center; margin-bottom: 20px;">
+        <a style="font-size: 20px; color: white;" href="">매칭 시작해볼래요!</a>
+    </div>
+    
+</div>
