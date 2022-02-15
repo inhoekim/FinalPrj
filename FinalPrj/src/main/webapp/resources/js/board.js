458f9a7e6766a2406b666bf5acd1224edc424a85
KimInHoe <inhoe.kim95@gmail.com>
2022-02-15 10:20:39 +0900

    css 픽셀 오류 수정

--- FinalPrj/src/main/webapp/resources/js/board.js
+++ FinalPrj/src/main/webapp/resources/js/board.js
@@ -3,5 +3,5 @@
 $(document).ready(function() {
 	$(".topbar_title").click(function(event){
 		$(event.target).siblings(".topbar_content").children(".content_wrapper").toggleClass("open");
-	})    
+	}) 
 })
