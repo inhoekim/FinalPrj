1a395e3a968d0186ccc2ff244d80471281275583
KimInHoe <inhoe.kim95@gmail.com>
2022-02-15 22:14:51 +0900

    댓글창 펼치기/숨기기 js 기능 추가

--- FinalPrj/src/main/webapp/resources/js/board.js
+++ FinalPrj/src/main/webapp/resources/js/board.js
@@ -3,5 +3,9 @@
 $(document).ready(function() {
 	$(".topbar_title").click(function(event){
 		$(event.target).siblings(".topbar_content").children(".content_wrapper").toggleClass("open");
-	}) 
+	});
+	
+	$(".like_detailBox").click(function(){
+        $(".like_detail").toggleClass("open");
+    });
 })
